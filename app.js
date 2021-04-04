d2e5157dfe990b7a3f88c938a586b6339a6e3dfa
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-04-03 23:41:33 -0400

    Sets up a mysql connection

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
+const mysql = require('mysql')
+const dbConfig = require('./dbconfig.json')
 
+const connection = mysql.createConnection({
+  host: dbConfig.db_host,
+  user: dbConfig.db_user,
+  password: dbConfig.db_password,
+  database: dbConfig.db_database,
+  port: dbConfig.db_port || 3306
+})
 
+connection.connect()
+
+connection.query('SELECT * FROM employee', (err, res, fields) => {
+  if (err) throw err
+
+  console.log(res)
+})
+
+connection.end()
 
 
 // Manage Departments
