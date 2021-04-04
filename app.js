616cc72c4e221e7134c8bf844252a0ab83e2bce8
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-04-03 23:00:15 -0400

    Getting started

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+
+
+// Manage Departments
+// Add
+// View
+// Delete
+
+// Manage Roles
+// Add
+// View
+// Update
+// Delete
+
+// Manage Employees
+// Add
+// View
+// Update
+// Delete
+// Change Managers
+
+
+// Helpers
+// View Total Budgets
